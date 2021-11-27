81859e0f35a4afeada680d4302a96b28756a5b1c
zhang <zhangs@lakeheadu.ca>
2021-11-27 08:26:48 -0500

    287-Find Duplicate

---
+++ src/findDuplicate.java
@@ -0,0 +1,19 @@
+class findDuplicate {
+    public int find(int[] nums){
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return fast;
+    }
+}
