cc704159c1b1a3731aff7c81903d279170b19941
zhang <zhangs@lakeheadu.ca>
2021-12-04 20:11:33 -0500

    668-Kth Smallest Number in Multiplication Table

---
+++ src/kthSmallestMultiplication.java
@@ -0,0 +1,17 @@
+class kthSmallestMultiplication {
+
+    public static int findKSmallest(int m, int n, int k){
+        int l = 1, r = m*n;
+        while(l < r){
+            int mid = l + (r - l) / 2, cnt = 0;
+            // count how many numbers are smaller than n
+            for(int i = 1; i <= m; i++){
+                cnt += Math.min(n, mid / i);
+            }
+            if(cnt < k){
+                l = mid + 1;
+            }else r = mid;
+        }
+        return l;
+    }
+}
