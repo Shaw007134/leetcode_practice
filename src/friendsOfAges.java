fd8e045cd5acbea349b07be4d99138f8f3237a8c
zhang <zhangs@lakeheadu.ca>
2021-12-04 21:30:30 -0500

    825-Friends of Appropriate Ages

---
+++ src/friendsOfAges.java
@@ -0,0 +1,17 @@
+public class friendsOfAges {
+
+    public static int findFriends(int[] ages){
+        int max_age = 120, res = 0;
+        int[] count = new int[max_age + 1], sum = new int[max_age + 1];
+        for(int i = 0; i < ages.length; i++){
+            count[ages[i]]++;
+        }
+        for (int i = 1; i <= max_age; i++) {
+            sum[i] = sum[i - 1] + count[i];
+        }
+        for (int i = 15; i <= max_age; i++) {
+            res += count[i] * (sum[Math.min(max_age, 2*i -15)] - sum[i - 1] - 1);
+        }
+        return res;
+    }
+}
