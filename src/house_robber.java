da008702278349d0723b1b4b7063d512f9b0bffb
zhang <zhangs@lakeheadu.ca>
2021-12-25 22:37:38 -0500

    213. House Robber II

--- src/house_robber.java
+++ src/house_robber.java
@@ -1,12 +1,20 @@
 class house_robber {
-    public int rob(int[] nums){
-        int n = nums.length;
+    public int rob1(int[] nums, int l, int r){
+//        int n = nums.length;
         int prepre = 0, pre = 0, now = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = l; i <= r; i++) {
             now = Math.max(pre, prepre + nums[i]);
             prepre = pre;
             pre = now;
         }
         return now;
+    }
+
+    public int rob(int[] nums){
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int a = rob1(nums, 0, n - 2);
+        int b = rob1(nums, 1, n - 1);
+        return Math.max(a, b);
     }
 }
