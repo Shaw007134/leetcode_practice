61aa98bfc1650b21272a68c64743b6f4c29030bb
zhang <zhangs@lakeheadu.ca>
2021-12-25 19:28:57 -0500

    canJump

--- src/jump.java
+++ src/jump.java
@@ -1,15 +1,22 @@
 class jump {
     public boolean canJump(int[] nums){
+//        int n = nums.length;
+//        int[] dp = new int[n];
+//        dp[0] = 1;
+//        for (int i = 0; i < n; i++) {
+//            if(dp[i] == 0) return false;
+//            if(i + nums[i] >= n - 1) return true;
+//            for (int j = i + 1; j <= i + nums[i]; j++) {
+//                dp[j] = 1;
+//            }
+//        }
+//        return false;
         int n = nums.length;
-        int[] dp = new int[n];
-        dp[0] = 1;
+        int max = 0;
         for (int i = 0; i < n; i++) {
-            if(dp[i] == 0) return false;
-            if(i + nums[i] >= n - 1) return true;
-            for (int j = i + 1; j <= i + nums[i]; j++) {
-                dp[j] = 1;
-            }
+            if(i > max) return false;
+            max = Math.max(max, i + nums[i]);
         }
-        return false;
+        return max >= n - 1;
     }
 }
